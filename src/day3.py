2bda98d725d1d97e7f55744c56bda740f5140805
Gilad Peleg <giladp007@gmail.com>
2017-12-04 15:21:43 +0200

    Advent day 3 part 2

--- src/day3.py
+++ src/day3.py
@@ -50,4 +50,45 @@ def part1():
     print sum([abs(x - starting_x), abs(y - starting_y)])
 
 
+def calculate_cell(square, x, y):
+    # need to look around the cell to add numbers
+    items = [
+        square[x - 1][y - 1],
+        square[x][y - 1],
+        square[x + 1][y - 1],
+        square[x - 1][y],
+        square[x + 1][y],
+        square[x - 1][y + 1],
+        square[x][y + 1],
+        square[x + 1][y + 1]
+    ]
+    values = [item for item in items if item]
+
+    around_sum = sum(values)
+
+    return around_sum
+
+
+def part2():
+    square_size = int(ceil(sqrt(INPUT_NUMBER)))
+    square_matrix = [[None] * square_size for _ in range(square_size)]
+
+    # find center
+    starting_x = starting_y = x = y = square_size // 2
+    # fill initial 1
+    square_matrix[y][x] = 1
+
+    for index, step in enumerate(steps_from_center(), start=2):
+        x += step.dx
+        y += step.dy
+
+        value = calculate_cell(square_matrix, x, y)
+        square_matrix[x][y] = value
+        if value > INPUT_NUMBER:
+            break
+
+    print value
+
+
 part1()
+part2()
